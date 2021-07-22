3d6de704a0875c2aee9bcdbcfb8bb8acac2c05a7
Jack Kellett <jackkellett02@gmail.com>
2021-07-22 20:46:20 +0100

    Test Conveyor Made

---
+++ ConveyorGame/Assets/Scripts/ConveyorScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorScript : MonoBehaviour {
+	#region Variables to assign via the unity inspector (Serialize Fields)
+	[SerializeField]
+	private GameObject directionToMoveInObject = null;
+
+	[SerializeField]
+	private float speed = 10.0f;
+	#endregion
+
+	#region Variable Declarations
+	private Rigidbody rBody = null;
+
+	private Vector3 back = Vector3.back;
+	#endregion
+
+	#region Private Functions
+	// Start is called before the first frame update
+	void Start() {
+		rBody = gameObject.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		Vector3 pos = rBody.position;
+		back = -(directionToMoveInObject.transform.position - pos);
+		rBody.position += back * speed * Time.fixedDeltaTime;
+		rBody.MovePosition(pos);
+	}
+	#endregion
+
+	#region Public Access Functions (Getters and Setters)
+
+	#endregion
+}
