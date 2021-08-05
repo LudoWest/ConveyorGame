c629f95658f992437e8aa95cf587aae07f9d25e0
Jack Kellett <jackkellett02@gmail.com>
2021-08-05 02:07:03 +0100

    Added stuff to main scene to play with.
    DIdn't manage to add the score stuff though

---
+++ ConveyorGame/Assets/Scripts/ItemSpawnerScript.cs
@@ -0,0 +1,80 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/// File Name:            ItemSpawnerScript.cs
+/// Author:               Jack Kellett
+/// Date Created:         04/08/2021
+/// Purpose:              To spawn the items onto the conveyor belt that the user has to pick up.
+/////////////////////////////////////////////////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnerScript : MonoBehaviour {
+	#region Variables to assign via the unity inspector (Serialize Fields)
+	[SerializeField]
+	private GameObject itemOne = null;
+
+	[SerializeField]
+	private GameObject itemTwo = null;
+
+	[SerializeField]
+	[Range(0.5f, 10.0f)]
+	private float spawnRate = 0.5f;
+
+	[SerializeField]
+	private Transform posOne = null;
+
+	[SerializeField]
+	private Transform posTwo = null;
+	#endregion
+
+	#region Private Variable Declarations
+	private bool cooldownOver = true;
+	private bool spawnInPosOne = true;
+
+	private Transform spawnPos = null;
+	#endregion
+
+	#region Private Functions
+	// Start is called before the first frame update
+	void Start() {
+		spawnPos = posOne;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (cooldownOver) {
+			UpdateSpawnPos();
+			SpawnRandomItem();
+			cooldownOver = false;
+			StartCoroutine("ItemSpawnCooldown");
+		}
+	}
+
+	private void UpdateSpawnPos() {
+		int randomNumber = Random.Range(0, int.MaxValue);
+		if ((randomNumber % 2) == 0) {
+			spawnPos = posOne;
+		} else {
+			spawnPos = posTwo;
+		}
+	}
+
+	private void SpawnRandomItem() {
+		int randomNumber = Random.Range(0, int.MaxValue);
+		if ((randomNumber % 2) == 0) {
+			Instantiate(itemOne, spawnPos);
+		} else {
+			Instantiate(itemTwo, spawnPos);
+		}
+	}
+
+	private IEnumerator ItemSpawnCooldown() {
+		yield return new WaitForSeconds(spawnRate);
+		cooldownOver = true;
+	}
+	#endregion
+
+	#region Public Access Functions
+
+	#endregion
+}
